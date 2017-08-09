022bd634b06a0b6008df5f1ae891fc0a348e8335
swethaganapuram <30767524+swethaganapuram@users.noreply.github.com>
2017-08-09 14:53:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,40 +22,41 @@ var articleOne = {
         </p>`
 }
 
-function createTemplate(data){
+function createTemplate (data) {
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
     var content = data.content;
+     
     var  htmltemplate = `
- <html>
-    <head>
-        <title>
+    <html>
+      <head>
+         <title>
             ${title}
-        </title>
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-    <div>
-        <a href="/">Home</a>
-    </div>    
-    <hr/>
-    <h3>
-        ${heading}
-    </h3>
-    <div>
+         </title>
+          <meta name="viewport" content="width=device-width,initial-scale=1">
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head>
+      <body>
+         <div class="container">
+      <div>
+         <a href="/">Home</a>
+      </div>    
+      <hr/>
+     <h3>
+         ${heading}
+     </h3>
+     <div>
         ${date}
-    </div>
-    <div>
-     ${content}
-    </div>
-    </div>
-    </body>
-</html>
-`;
- return htmlTemplate;
+     </div>
+     <div>
+      ${content}
+     </div>
+     </div>
+     </body>
+    </html>
+   `;
+    return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
