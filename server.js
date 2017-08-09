78ce2eec50b095d4d2481018a4fffda827554f02
swethaganapuram <30767524+swethaganapuram@users.noreply.github.com>
2017-08-09 12:57:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/aticle-one',function(req,req){
+app.get('/aticle-one',function(req,res){
     res.send('Article one requested and will be served here');
 });
 
-app.get('/aticle-two',function(req,req){
+app.get('/aticle-two',function(req,res){
     res.send('Article two requested and will be served here');
 });
 
-app.get('/aticle-three',function(req,req){
+app.get('/aticle-three',function(req,res){
     res.send('Article three requested and will be served here');
 });
 
