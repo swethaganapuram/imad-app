cfea7939722384270b26cd5a548422005624067b
swethaganapuram <30767524+swethaganapuram@users.noreply.github.com>
2017-08-09 15:02:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ function createTemplate (data) {
     var heading = data.heading;
     var content = data.content;
      
-    var  htmltemplate = `
+    var  htmlTemplate = `
     <html>
       <head>
          <title>
