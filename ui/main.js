0a3fb4b032c3ed144c737bfd67ce079e2b9b16ce
swethaganapuram <30767524+swethaganapuram@users.noreply.github.com>
2017-08-15 13:35:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var button = document.getElementById('counter');
 button.onclick = function (){
     
     //create a request object
-    var request = newXMLHttpRequest();
+    var request = new XMLHttpRequest();
     
     //capture the response and store it in a variable
     request.onreadystatechange = function (){
